86d0d1ffabb007dfdb034fd0d70784105eb63fdd
Benjamin <benja.mora.torres@gmail.com>
2019-09-28 05:53:07 -0300

    update

--- Controlador/Cpregunta.php
+++ Controlador/Cpregunta.php
@@ -7,7 +7,11 @@ require_once($rootDir . "/DAO/PreguntaDAO.php");
 if(isset($_POST['opcion'])){
 	$opc=htmlspecialchars($_POST['opcion']);
 	if($opc=="agregar"){
-
+		$opc=htmlspecialchars($_POST['rut']);
+		$opc=htmlspecialchars($_POST['nombre']);
+		$opc=htmlspecialchars($_POST['apellido']);
+		$opc=htmlspecialchars($_POST['correo']);
+		$opc=htmlspecialchars($_POST['sexo']);
 	}
 	elseif($opc=="buscar"){
 
